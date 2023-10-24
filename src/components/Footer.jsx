695200d7ff49fd7fe801fc17dcb62d7c9e1b94e2
mr2yesung <mryesung@naver.com>
2023-10-23 23:12:53 -0700

    add footer, modify all fonts to be equal to roboto condensed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+  return (
+    <>
+      <p>Made by Yeseong Kim</p>
+      <p>
+        ©{" "}
+        <a
+          href="https://github.com/mr2yesung/QR-Generator"
+          className="github-link"
+        >
+          https://github.com/mr2yesung/QR-Generator
+        </a>
+      </p>
+    </>
+  );
+}
+
+export default Footer;
